a20c26391af5f5e936077081e5a21cddf86cf268
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-09-18 10:28:24 -0300

    feat: get all book by title use case

---
+++ src/use-cases/library/book/get-book-by-title-use-case.ts
@@ -0,0 +1,26 @@
+import { Book } from "@/entities/Book";
+import { BookRepository } from "@/repositories/book-repository";
+import { NotFoundError } from "@/use-cases/errors/not-found-error";
+
+interface GetBookByTitleRequest {
+  title: string;
+  libraryId: string
+}
+
+interface GetBookByTitleResponse {
+  book: Book | NotFoundError;
+}
+
+export class GetBookByTitleUseCase {
+  constructor(private bookRepository: BookRepository) { }
+
+  async execute({ title, libraryId }: GetBookByTitleRequest): Promise<GetBookByTitleResponse> {
+    const book = await this.bookRepository.findByTitle(title, libraryId);
+
+    if (!book) {
+      throw new NotFoundError()
+    }
+
+    return { book };
+  }
+}
