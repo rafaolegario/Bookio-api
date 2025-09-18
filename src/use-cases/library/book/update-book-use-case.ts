189f2117f72f419a2080e43f026b3c21cb4a49a2
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-09-18 10:28:40 -0300

    feat: update book use case

---
+++ src/use-cases/library/book/update-book-use-case.ts
@@ -0,0 +1,51 @@
+import { Book, BookGenders } from "@/entities/Book";
+import { BookRepository } from "@/repositories/book-repository";
+import { NotAllowedError } from "@/use-cases/errors/not-allowed-error";
+
+interface UpdateBookRequest {
+  bookId: string;
+  title?: string;
+  author?: string;
+  imageUrl?: string;
+  gender?: BookGenders;
+  year?: Date;
+  available?: number;
+  total_loans?: number;
+}
+
+interface UpdateBookResponse {
+  book: Book;
+}
+
+export class UpdateBookUseCase {
+  constructor(private bookRepository: BookRepository) { }
+
+  async execute({
+    bookId,
+    title,
+    author,
+    imageUrl,
+    gender,
+    year,
+    available,
+    total_loans,
+  }: UpdateBookRequest): Promise<UpdateBookResponse> {
+    const book = await this.bookRepository.findById(bookId);
+
+    if (!book) {
+      throw new NotAllowedError();
+    }
+
+    if (title) book.setTitle(title);
+    if (author) book.setAuthor(author);
+    if (imageUrl) book.setImageUrl(imageUrl);
+    if (gender) book.setGender(gender);
+    if (year) book.setYear(year);
+    if (available !== undefined) book.setAvailable(available);
+    if (total_loans !== undefined) book.setTotalLoans(total_loans);
+
+    await this.bookRepository.save(book);
+
+    return { book };
+  }
+}
