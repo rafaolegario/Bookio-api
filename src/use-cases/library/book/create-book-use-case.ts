c01e7c8af8555f250f6d20bd95545e1ecc82e7b2
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-09-18 10:27:37 -0300

    feat: create book use case

---
+++ src/use-cases/library/book/create-book-use-case.ts
@@ -0,0 +1,52 @@
+import { Book, BookGenders } from "@/entities/Book";
+import { BookRepository } from "@/repositories/book-repository";
+import { NotAllowedError } from "@/use-cases/errors/not-allowed-error";
+import { UniqueEntityId } from "@/entities/UniqueEntityId";
+
+interface CreateBookRequest {
+  author: string;
+  libraryId: string;
+  title: string;
+  imageUrl: string;
+  gender: BookGenders;
+  year: Date;
+  available: number;
+}
+
+export class CreateBookUseCase {
+  constructor(private bookRepository: BookRepository) { }
+
+  async execute({
+    author,
+    libraryId,
+    title,
+    imageUrl,
+    gender,
+    year,
+    available,
+  }: CreateBookRequest) {
+    const existingBook = await this.bookRepository.findByTitle(
+      title,
+      libraryId
+    );
+
+    if (existingBook) {
+      throw new NotAllowedError();
+    }
+
+    const book = new Book({
+      libraryId: new UniqueEntityId(libraryId),
+      author,
+      title,
+      imageUrl,
+      gender,
+      year,
+      available,
+      total_loans: 0,
+    });
+
+    await this.bookRepository.create(book);
+
+    return book;
+  }
+}
